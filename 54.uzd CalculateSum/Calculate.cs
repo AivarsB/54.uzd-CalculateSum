5abe688f3a415cb69f0118a43d863e0539c08329
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:35:12 +0200

    Add project files.

---
+++ 54.uzd CalculateSum/Calculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _54.uzd_CalculateSum
+{
+    public class Calculate
+    {
+        public static int CalculateSum(int x, int y)
+        {
+            var z = x + y;
+            return z;
+        }
+    }
+}
---
+++ 54.uzd CalculateSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _54.uzd_CalculateSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Enter x: ");
+                var x = int.Parse(Console.ReadLine());
+                Console.Write("Enter y: ");
+                var y = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Calculate.CalculateSum(x, y));
+
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+
+            } while (true);
+        }
+    }
+}
+// Izveidot static metodi CalculateSum atsevišķā failā. Metodei , kā parametri ir jāsaņem divas int vērtības x un y.
+// Šai metodei ir jāatgriež šo skaitļu summa. Šo metodi ir jāizmanto vairākas reizes galvenajā programmā.
